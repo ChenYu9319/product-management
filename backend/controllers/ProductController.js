1f38b396a39837c3794afa81eb119f901bb39db3
Sky Lim Le Tian <deathkongkong@gmail.com>
2026-09-23 10:40:25 +0800

    done

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -9,3 +9,21 @@ exports.getProductById = async (req, res) => {
     const product = await Product.findOne({ _id: req.params.id })
     res.json(product)
 }
+
+exports.addNewProduct = async (req, res) => {
+    const newProduct = new Product(req.body)
+    await newProduct.save()
+    res.json(newProduct)
+}
+
+exports.updateProduct = async (req, res) => {
+    const { id } = req.params
+    const editedProduct = await Product.findOneAndUpdate({_id: id}, req.body, { new: true })
+    res.json(editedProduct)
+}
+
+exports.deleteProduct = async (req, res) => {
+    const { id } = req.params
+    await Product.findOneAndDelete({_id: id})
+    res.status(204).json()
+}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -9,4 +9,10 @@ router.get("/", auth.authenticate, productController.getAllProducts)
 
 router.get("/:id", auth.authenticate, productController.getProductById)
 
+router.post("/", auth.authenticate, productController.addNewProduct)
+
+router.patch("/:id", auth.authenticate, productController.updateProduct)
+
+router.delete("/:id", auth.authenticate, productController.deleteProduct)
+
 module.exports = router
